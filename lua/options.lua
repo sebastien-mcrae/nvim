c219d335bd940b0d2851809cf2efb6d3bb97d955
Sebastien McRae <sebastien.mcrae@trydatabook.com>
2022-12-10 13:41:55 -0800

    Update options

--- lua/options.lua
+++ lua/options.lua
@@ -12,7 +12,7 @@ end
 
 -- auto-reload files when modified externally
 -- https://unix.stackexchange.com/a/383044
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
   command = "if mode() != 'c' | checktime | endif",
   pattern = { "*" },
 })
