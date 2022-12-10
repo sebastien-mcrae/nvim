38d104555bbbcc9b922dd52e19346c8c46594996
Sebastien McRae <sebastien.mcrae@trydatabook.com>
2022-12-10 13:42:07 -0800

    Update LSP

--- lua/setup/cmp.lua
+++ lua/setup/cmp.lua
@@ -59,9 +59,3 @@
     })
   })
 
-  -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
-  }
